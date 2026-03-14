5a227ebd5a07a30ccebc34b0d9457421997a52e2
Teodosi <teo.kachakov@gmail.com>
2026-03-14 18:19:41 +0200

    feat: add navigation TypeScript type definitions

---
+++ src/navigation/types.ts
@@ -0,0 +1,42 @@
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import type { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+import type { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
+
+export type AuthStackParamList = {
+  Login: undefined;
+  Register: undefined;
+};
+
+export type MainTabsParamList = {
+  Browse: undefined;
+  MyMovies: undefined;
+  Upcoming: undefined;
+  Profile: undefined;
+};
+
+export type RootStackParamList = {
+  Auth: NavigatorScreenParams<AuthStackParamList>;
+  Main: NavigatorScreenParams<MainTabsParamList>;
+  MovieDetails: { movieId: string };
+};
+
+export type RootStackScreenProps<T extends keyof RootStackParamList> =
+  NativeStackScreenProps<RootStackParamList, T>;
+
+export type AuthStackScreenProps<T extends keyof AuthStackParamList> =
+  CompositeScreenProps<
+    NativeStackScreenProps<AuthStackParamList, T>,
+    RootStackScreenProps<keyof RootStackParamList>
+  >;
+
+export type MainTabScreenProps<T extends keyof MainTabsParamList> =
+  CompositeScreenProps<
+    BottomTabScreenProps<MainTabsParamList, T>,
+    RootStackScreenProps<keyof RootStackParamList>
+  >;
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
