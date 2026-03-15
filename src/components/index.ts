e4f65afe484b2ee1a7677f09d8eb8d65248a4569
Teodosi <teo.kachakov@gmail.com>
2026-03-15 13:15:14 +0200

    chore: align folder structure

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as LoadingScreen } from './LoadingScreen';
+export { default as ErrorScreen } from './ErrorScreen';
+export { default as EmptyState } from './EmptyState';
+export { default as FormField } from './FormField';
+export { default as PrimaryButton } from './PrimaryButton';
+export { default as ErrorBoundary } from './ErrorBoundary';
---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useAppNavigation } from './useAppNavigation';
+export { useMovies } from './useMovies';
+export { useUpcoming } from './useUpcoming';
+export { useMovieDetails } from './useMovieDetails';
+export { useImagePicker } from './useImagePicker';
